e7f0d60e4016ac4482266e7f4a80a00679430f36
Santi Aguilera <santiago.aguilera@mercadolibre.com>
2021-09-26 01:42:54 -0300

    Minor changes from ec2 instance

--- cron.sh
+++ cron.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cd /home/ec2-user/il-monitor
-npm run monitor
+#####
+# If using the cron in a Linux instance run before:
+#
+# > which node
+# > sudo ln -s <output of which node> /usr/bin/node
+# > npm install -g ts-node
+# > which ts-node
+# > sudo ln -s <output of which ts-node> /usr/bin/ts-node
+#####
+
+cd /home/ec2-user/impermanent-loss-monitor
+/home/ec2-user/.nvm/versions/node/v16.6.1/bin/npm run monitor
